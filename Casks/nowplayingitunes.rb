e00fdfd78c2d9a5fbf9738f1a28ea0f8021b9bcf
on0z <onoz.dev@gmail.com>
2019-11-13 01:39:25 +0900

    NowPlaying iTunesを追加

---
+++ Casks/nowplayingitunes.rb
@@ -0,0 +1,10 @@
+cask 'nowplayingitunes' do
+    version '1.13'
+    sha256 'bf8cdbbba020e3707b5a13c97ea5b8df2477d977443ca4734fe91906a2632254'
+
+    url 'https://github.com/on0z/homebrew-on0zbrew/Archives/NowPlaying iTunes.app.zip'
+    name 'NowPlaying iTunes'
+    homepage 'https://github.com/on0z/homebrew-on0zbrew/Archives/NowPlaying iTunes.md'
+
+    app 'NowPlaying iTunes.app'
+end
