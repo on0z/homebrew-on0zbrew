0ca9f085c4156023055393c8a4603c1a649410e3
on0z <onoz.dev@gmail.com>
2019-11-13 01:56:54 +0900

    Update nowplayingitunes.rb
    NowPlaying iTunesのハッシュ誤りを修正

--- Casks/nowplayingitunes.rb
+++ Casks/nowplayingitunes.rb
@@ -1,6 +1,6 @@
 cask 'nowplayingitunes' do
     version '1.13'
-    sha256 'bf8cdbbba020e3707b5a13c97ea5b8df2477d977443ca4734fe91906a2632254'
+    sha256 '3397d0f8fba22eba4087dce56ba237b88972e32d6ad30f58aefa1e783cec041e'
 
     url 'https://github.com/on0z/homebrew-on0zbrew/blob/master/Archives/NowPlaying%20iTunes.app.zip'
     name 'NowPlaying iTunes'
