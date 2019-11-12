603682837d15f75a4e76d6eed8ab6989e83f19ba
on0z <onoz.dev@gmail.com>
2019-11-13 01:39:12 +0900

    SyncWalkmanを追加

---
+++ Casks/syncwalkman.rb
@@ -0,0 +1,10 @@
+cask 'syncwalkman' do
+    version '4.0'
+    sha256 'b86e417017189b7e84cb970769f6d16d17c02d11009ba248257f298ce4430c4e'
+
+    url 'https://github.com/on0z/SyncWalkman/releases/download/gui%2F4.x%2F4.0/SyncWalkman-gui.app.zip'
+    name 'SyncWalkman'
+    homepage 'https://github.com/on0z/SyncWalkman'
+
+    app 'SyncWalkman-gui.app'
+end
