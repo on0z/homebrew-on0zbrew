7f18e967eca17167f4a3dcc913fb9d7059b664f1
on0z <onoz.dev@gmail.com>
2019-11-13 01:50:16 +0900

    NowPlaying iTunesをインストールできない問題を修正

--- Casks/nowplayingitunes.rb
+++ Casks/nowplayingitunes.rb
@@ -2,9 +2,9 @@ cask 'nowplayingitunes' do
     version '1.13'
     sha256 'bf8cdbbba020e3707b5a13c97ea5b8df2477d977443ca4734fe91906a2632254'
 
-    url 'https://github.com/on0z/homebrew-on0zbrew/Archives/NowPlaying iTunes.app.zip'
+    url 'https://github.com/on0z/homebrew-on0zbrew/blob/master/Archives/NowPlaying%20iTunes.app.zip'
     name 'NowPlaying iTunes'
-    homepage 'https://github.com/on0z/homebrew-on0zbrew/Archives/NowPlaying iTunes.md'
+    homepage 'https://github.com/on0z/homebrew-on0zbrew/blob/master/Archives/NowPlaying%20iTunes.md'
 
     app 'NowPlaying iTunes.app'
 end
