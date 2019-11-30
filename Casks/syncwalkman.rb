f125663e8dc71043d92963717f15d8bff2976724
on0z <onoz.dev@gmail.com>
2019-12-01 00:02:15 +0900

    Updated syncwalkman.rb

--- Casks/syncwalkman.rb
+++ Casks/syncwalkman.rb
@@ -1,8 +1,8 @@
 cask 'syncwalkman' do
-    version '4.0'
-    sha256 'b86e417017189b7e84cb970769f6d16d17c02d11009ba248257f298ce4430c4e'
+    version '5.1'
+    sha256 'ad9d633d7f272276468bb655f4d73e1e17d166a81f3798471ccca5496bb52a6b'
 
-    url 'https://github.com/on0z/SyncWalkman/releases/download/gui%2F4.x%2F4.0/SyncWalkman-gui.app.zip'
+    url 'https://github.com/on0z/SyncWalkman/releases/download/gui%2F5.x%2F5.1/SyncWalkman-gui.app.zip'
     name 'SyncWalkman'
     homepage 'https://github.com/on0z/SyncWalkman'
 
