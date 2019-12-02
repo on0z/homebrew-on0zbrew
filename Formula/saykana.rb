ba9db966486c4e83cd152b646e38f7a52cf043c9
on0z <onoz.dev@gmail.com>
2019-12-02 17:42:54 +0900

    Add saykana.rb

---
+++ Formula/saykana.rb
@@ -0,0 +1,37 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Saykana < Formula
+  homepage "https://www.a-quest.com/quickware/saykana/"
+  url "https://github.com/on0z/homebrew-on0zbrew/raw/master/Archives/saykana.tar.gz"
+  version "1.11"
+  sha256 "bbf719290a332acbadda8d671423db30f238606cc431240656b4696d86a6463f"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    
+    bin.install 'SayKana'
+
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test saykana`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    # system "false"
+  end
+end
