3c02b655d76586224f5153dc89a5a49af3b4ad7c
on0z <onoz.dev@gmail.com>
2021-02-01 14:06:51 +0900

    Update nowplayingitunes
    iTunesLibrary使用理由が未記載だったためか，起動できなかった

--- Casks/nowplayingitunes.rb
+++ Casks/nowplayingitunes.rb
@@ -1,6 +1,6 @@
 cask 'nowplayingitunes' do
     version '2.0'
-    sha256 'e4f29b20118cca454247c59518dd9e814321ee70fa0950b9cc7bd3ca50cd52d8'
+    sha256 'cb5f8f7df108ef967717929a674354076b21e114efa4fff9d6a1112f898808cd'
 
     url 'https://github.com/on0z/homebrew-on0zbrew/raw/master/Archives/NowPlaying%20iTunes.app.zip'
     name 'NowPlaying iTunes'
